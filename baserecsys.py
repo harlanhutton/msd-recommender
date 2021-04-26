485fd52d5feadff1acf46b4e320282a35840fb88
Harlan Hutton <harlanhutton@a-10-27-11-185.dynapool.vpn.nyu.edu>
2021-04-26 17:36:38 -0400

    hi

--- baserecsys.py
+++ baserecsys.py
@@ -2,6 +2,21 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
-lines = spark.read.parquet(file_path)
-parts = lines.map(lambda row: row.value.split("::"))
-print(parts)
+
+def main(spark, file_path):
+
+	lines = spark.read.parquet(file_path)
+	lines.createOrReplaceTempView('lines')
+	print(lines[1:10])
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('part2').getOrCreate()
+
+    # Get file_path for dataset to analyze
+    file_path = sys.argv[1]
+
+    main(spark, file_path)
