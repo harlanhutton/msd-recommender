2e15dcaea6cb1dffc347e8e36230233ee12f859d
Jenna Eubank <jennaeubank@a-10-27-17-106.dynapool.vpn.nyu.edu>
2021-04-26 23:45:08 -0400

    pq file write working version 1 percent sample

--- baserecsys_pq.py
+++ baserecsys_pq.py
@@ -24,12 +24,12 @@ def main(spark, file_path):
     df = lines.sample(fraction=0.01, seed = 1)
     print(type(df), 'dftype')
     print(df.count(), len(df.columns), 'shape')
-    df.write.mode('overwrite').parquet(f'hdfs:/user/jke261/test01sample.parquet')
+    df.write.mode('overwrite').parquet(f'hdfs:/user/jke261/train_sample01.parquet')
     
     
-    re_read = spark.read.parquet('hdfs:/user/jke261/test01sample.parquet')
-    re_read.createOrReplaceTempView('re_read')
-    print(re_read.head(20))
+#     re_read = spark.read.parquet('hdfs:/user/jke261/train_sample01.parquet')
+#     re_read.createOrReplaceTempView('re_read')
+#     print(re_read.head(20))
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
