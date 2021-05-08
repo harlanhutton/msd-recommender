0d256d65399cc5a8332dd8607c184871d0b8d1ab
Jenna Eubank <jennaeubank@a-10-27-18-227.dynapool.vpn.nyu.edu>
2021-05-07 20:44:23 -0400

    metadata bd sampler v3

--- sampler_db.py
+++ sampler_db.py
@@ -8,22 +8,22 @@ from pyspark.sql import SparkSession
 import numpy as np
 import pandas as pd
 
+from pyspark.sql import SQLContext
+
 
 def main(spark, netID):
     
-    filer.createOrReplaceTempView('/scratch/work/public/MillionSongDataset/')
-    
-    sampler = spark.sql('SELECT * FROM filer ORDER BY RANDOM() LIMIT 2000')
+    sampler = spark.sql("SELECT * FROM db.`/scratch/work/public/MillionSongDataset/AdditionalFiles/track_metadata.db ` ORDER BY RANDOM() LIMIT 2000")
     
     sampler.write.mode('overwrite').parquet(f'hdfs:/user/{netID}/meta_db_sample.parquet')
 
-
+    
 # Only enter this block if we're in main
 if __name__ == "__main__":
 
     # Create the spark session object
     spark = SparkSession.builder.appName('sampler_db').getOrCreate()
-    
+
     netID = getpass.getuser()
 
     main(spark, netID)
