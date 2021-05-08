bc5aebbfa4d83b1fb1eca041e2e7ebf0290ee4e2
Jenna Eubank <jennaeubank@a-10-27-18-227.dynapool.vpn.nyu.edu>
2021-05-07 20:13:42 -0400

    metadata bd sampler v1

---
+++ sampler_db.py
@@ -0,0 +1,31 @@
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+import getpass
+import sys
+
+from pyspark.sql import SparkSession
+import numpy as np
+import pandas as pd
+
+
+def main(spark, netID):
+    
+    filer = '/scratch/work/public/MillionSongDataset/'
+    
+    filer.createOrReplaceTempView('filer')
+    
+    sampler = spark.sql('SELECT * FROM filer ORDER BY RANDOM() LIMIT 2000')
+    
+    sampler.write.mode('overwrite').parquet(f'hdfs:/user/{netID}/meta_db_sample.parquet')
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('sampler_db').getOrCreate()
+    
+    netID = getpass.getuser()
+
+    main(spark, netID)
