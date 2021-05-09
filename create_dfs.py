a066da03b2ae33bb0f9ba7c7c354c2ecacffb276
Harlan Hutton <harlanhutton@b-10-27-78-231.dynapool.vpn.nyu.edu>
2021-05-09 12:59:09 -0600

    updating create df

--- create_dfs.py
+++ create_dfs.py
@@ -5,13 +5,12 @@ import sys
 import getpass
 
 
-def main(spark, sc, train_input, test_input, val_input,user_id):
-    
-          
+def main(spark):
+           
     # read in data
-    trainSample = spark.read.parquet(train_input)
-    testSample = spark.read.parquet(test_input)
-    valSample = spark.read.parquet(val_input)
+    trainSample = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train.parquet')
+    testSample = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_test.parquet')
+    valSample = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_validation.parquet')
     
     valSample.createOrReplaceTempView('valSample')
     trainSample.createOrReplaceTempView('trainSample')
@@ -21,24 +20,24 @@ def main(spark, sc, train_input, test_input, val_input,user_id):
     indexer_obj_1 = StringIndexer(inputCol="user_id", outputCol="user_id_numer").setHandleInvalid("keep")
     indexer_model_1 = indexer_obj_1.fit(trainSample)
     indexer_df_1 = indexer_model_1.transform(trainSample)
-    print('finised indexer 1 on train')
+    print('finished indexer 1 on train')
 
     indexer_obj_2 = StringIndexer(inputCol="track_id", outputCol="track_id_numer").setHandleInvalid("keep")
     indexer_model_2 = indexer_obj_2.fit(indexer_df_1)
     indexer_df_2 = indexer_model_2.transform(indexer_df_1)
-    print('finised indexer 2 on train')
+    print('finished indexer 2 on train')
 
     train_df = indexer_df_2.drop('user_id')
     train_df = train_df.drop('track_id')
-    train_df = train_df.repartition(2000)
+    train_df = train_df.repartition(5000)
     
     print('dropped columns in training set')
 
     val_df_1 = indexer_model_1.transform(valSample)
-    ('transform validation set with indexer 1')
+    print('transform validation set with indexer 1')
     
     val_df_2= indexer_model_2.transform(val_df_1)
-    ('transform validation set with indexer 2')
+    print('transform validation set with indexer 2')
 
     val_df = val_df_2.drop('user_id')
     val_df= val_df.drop('track_id')
@@ -54,9 +53,9 @@ def main(spark, sc, train_input, test_input, val_input,user_id):
 
     test_df = test_df.repartition(5000)
 
-    test_df.write.mode('overwrite').parquet(f'hdfs:/user/{netID}/pub/test_df.parquet')
-    train_df.write.mode('overwrite').parquet(f'hdfs:/user/{netID}/pub/train_df.parquet')
-    val_df.write.mode('overwrite').parquet(f'hdfs:/user/{netID}/pub/val_df.parquet')
+    test_df.write.mode('overwrite').parquet(f'hdfs:/user/ahh303/pub/test_df.parquet')
+    train_df.write.mode('overwrite').parquet(f'hdfs:/user/ahh303/pub/train_df.parquet')
+    val_df.write.mode('overwrite').parquet(f'hdfs:/user/ahh303/pub/val_df.parquet')
 
 
 
@@ -64,14 +63,5 @@ def main(spark, sc, train_input, test_input, val_input,user_id):
 if __name__ == "__main__":
         # Create the spark session object
     spark = SparkSession.builder.appName('sampler').getOrCreate()
-
-    # Get file_path for dataset to analyze
-    train = sys.argv[1]
-    test = sys.argv[2]
-    val = sys.argv[3]
     
-    sc = spark.sparkContext
-    
-    netID = getpass.getuser()
-    
-    main(spark, sc, train, test, val, netID)
+    main(spark)
