15cc12388fc16f4dba9d8d97a92cbdf16943467c
Harlan Hutton <harlanhutton@a-10-27-16-142.dynapool.vpn.nyu.edu>
2021-05-10 09:34:23 -0600

    harlan new val

---
+++ val_df.py
@@ -0,0 +1,42 @@
+from pyspark.ml.feature import StringIndexer
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+import sys
+import getpass
+
+
+def main(spark):
+           
+    # read in data
+    trainSample = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train.parquet')
+    valSample = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_validation.parquet')
+
+#     # StringIndexer to create new columns and dataframes
+    indexer_obj_1 = StringIndexer(inputCol="user_id", outputCol="user_id_numer").setHandleInvalid("keep")
+    indexer_model_1 = indexer_obj_1.fit(trainSample)
+    indexer_df_1 = indexer_model_1.transform(trainSample)
+    print('finished indexer 1 on train')
+
+    indexer_obj_2 = StringIndexer(inputCol="track_id", outputCol="track_id_numer").setHandleInvalid("keep")
+    indexer_model_2 = indexer_obj_2.fit(indexer_df_1)
+    print('finished indexer 2 on train')
+
+    val_df_1 = indexer_model_1.transform(valSample)
+    print('transform validation set with indexer 1')
+    
+    val_df_2= indexer_model_2.transform(val_df_1)
+    print('transform validation set with indexer 2')
+
+    val_df = val_df_2.drop('user_id')
+    val_df= val_df.drop('track_id')
+    
+    val_df = val_df.repartition(5000)
+  
+    val_df.write.mode('overwrite').parquet('hdfs:/user/ahh303/pub/val_df.parquet')
+
+
+if __name__ == "__main__":
+        # Create the spark session object
+    spark = SparkSession.builder.appName('sampler').getOrCreate()
+    
+    main(spark)
